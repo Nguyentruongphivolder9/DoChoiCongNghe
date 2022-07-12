a8b06419bcac36c387d760ff0c6abe920aac1ce1
phi <nguyentruongphi15032003@gmail.com>
2022-07-12 17:40:18 +0700

    banner

--- app/src/main/java/com/example/dochoicongnghe/MainActivity.java
+++ app/src/main/java/com/example/dochoicongnghe/MainActivity.java
@@ -1,14 +1,87 @@
 package com.example.dochoicongnghe;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.ViewFlipper;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.CenterCrop;
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
+import com.bumptech.glide.request.RequestOptions;
+import com.google.android.material.navigation.NavigationView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+    ViewFlipper viewFlipper;
+    RecyclerView recyclerView;
+    NavigationView navigationView;
+    ListView listView;
+    DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Anhxa();
+        actionBar();
+        ActionViewFlipper();
+    }
+
+    private void ActionViewFlipper() {
+        List<String> mangquangcao = new ArrayList<>();
+        mangquangcao.add("https://cdn.tgdd.vn/2022/06/banner/720-220-720x220-199.png");
+        mangquangcao.add("https://cdn.tgdd.vn/2022/07/banner/720-220-720x220-7.png");
+        mangquangcao.add("https://cdn.tgdd.vn/2022/07/banner/720-220-720x220-75.png");
+        mangquangcao.add("https://cdn.tgdd.vn/2022/06/banner/720-220-720x220-225.png");
+        for (int i = 0; i < mangquangcao.size(); i++) {
+            ImageView imageView = new ImageView(getApplicationContext());
+            Glide.with(getApplicationContext())
+                    .load(mangquangcao.get(i))
+                    .apply(new RequestOptions().transform(new CenterCrop()).transform(new RoundedCorners(15)))
+                    .into(imageView);
+            imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+            viewFlipper.addView(imageView);
+        }
+        viewFlipper.setFlipInterval(3000);
+        viewFlipper.setAutoStart(true);
+        Animation slide_in = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.slide_in_right);
+        Animation slide_out = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.slide_out_right);
+        viewFlipper.setInAnimation(slide_in);
+        viewFlipper.setInAnimation(slide_out);
+    }
+
+    private void actionBar() {
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+        toolbar.setNavigationIcon(android.R.drawable.ic_menu_sort_by_size);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+    }
+
+    private void Anhxa() {
+        toolbar = findViewById(R.id.toolbarHome);
+        viewFlipper = findViewById(R.id.viewFlipperHome);
+        recyclerView = findViewById(R.id.recyclerviewHome);
+        navigationView = findViewById(R.id.navigationHome);
+        listView = findViewById(R.id.listviewHome);
+        drawerLayout = findViewById(R.id.drawerlayoutHome);
     }
 }
